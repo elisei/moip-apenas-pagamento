2dce5898238006572f8167e8435f630da1a95a94
Bruno <brunoelisei@gmail.com>
2018-07-25 12:55:14 -0300

    Informações adcionais para analise de risco.

--- app/code/local/MOIP/Transparente/Model/Api.php
+++ app/code/local/MOIP/Transparente/Model/Api.php
@@ -829,7 +829,7 @@ class MOIP_Transparente_Model_Api
                     )
                 ),
                 "device" => array(
-                    "device" => $order->getRemoteIp(),
+                    "ip" => $order->getRemoteIp(),
                     "userAgent" => Mage::helper('core/http')->getHttpUserAgent()
                     )
             );
@@ -846,7 +846,7 @@ class MOIP_Transparente_Model_Api
                     )
                 ),
                 "device" => array(
-                    "device" => $order->getRemoteIp(),
+                    "ip" => $order->getRemoteIp(),
                     "userAgent" => Mage::helper('core/http')->getHttpUserAgent()
                     )
             );
@@ -857,7 +857,7 @@ class MOIP_Transparente_Model_Api
 
 
 
-    public function setJsonBoleto()
+    public function setJsonBoleto($order)
     {
         
         $NDias = Mage::getStoreConfig('payment/moip_boleto/vcmentoboleto');
@@ -876,13 +876,17 @@ class MOIP_Transparente_Model_Api
                         "third" => "+ Info em: " . Mage::getBaseUrl()
                     )
                 )
-            )
+            ),
+                "device" => array(
+                    "ip" => $order->getRemoteIp(),
+                    "userAgent" => Mage::helper('core/http')->getHttpUserAgent()
+                    )
         );
 
         $json           = json_encode($json);
         return $json;
     }
-    public function setJsonTef($info)
+    public function setJsonTef($info, $order)
     {
         $additionaldata = unserialize($info->getAdditionalData());
         $NDias = Mage::getStoreConfig('payment/moip_boleto/vcmentotef');
@@ -896,7 +900,11 @@ class MOIP_Transparente_Model_Api
                     "expirationDate" => $this->getDataVencimento($NDias, $diasUteis),
                     "returnUri" => Mage::getBaseUrl()
                 )
-            )
+            ),
+                "device" => array(
+                    "ip" => $order->getRemoteIp(),
+                    "userAgent" => Mage::helper('core/http')->getHttpUserAgent()
+                    )
         );
         $json           = json_encode($json);
         return $json;
